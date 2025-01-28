7c97bf4c8a6defda0978e3000e372d24400b31b5
EsGaCristo <crjgarcia.e@gmail.com>
2025-01-27 22:35:16 -0700

    Base bussinesdomain -> customer,product

---
+++ bussinesdomain/product/src/main/java/com/paymentchain/product/controller/ProductRestController.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.paymentchain.product.controller;
+
+import com.paymentchain.product.entities.Product;
+import com.paymentchain.product.repository.ProductRepository;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+/**
+ *
+ * @author cristo
+ */
+@RestController
+@RequestMapping("/product")
+public class ProductRestController {
+    
+    @Autowired
+    ProductRepository customerRepository;
+    
+    @GetMapping()
+    public List<Product> list() {
+        return customerRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable long id) {
+         Optional<Product> product = customerRepository.findById(id);
+        if (product.isPresent()) {
+            return new ResponseEntity<>(product.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<?> put(@PathVariable long id, @RequestBody Product input) {
+         Optional<Product> optionalcustomer = customerRepository.findById(id);
+        if (optionalcustomer.isPresent()) {
+            Product newcustomer = optionalcustomer.get();
+            newcustomer.setName(input.getName());
+            newcustomer.setCode(input.getCode());
+             Product save = customerRepository.save(newcustomer);
+          return new ResponseEntity<>(save, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody Product input) {
+        Product save = customerRepository.save(input);
+        return ResponseEntity.ok(save);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+         customerRepository.deleteById(id);
+         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+}
