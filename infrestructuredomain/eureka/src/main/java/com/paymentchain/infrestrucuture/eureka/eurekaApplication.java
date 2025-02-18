321a7764674979f803fa6a49175383d40905cab3
EsGaCristo <crjgarcia.e@gmail.com>
2025-02-18 13:45:44 -0700

    avance eureka

--- infrestructuredomain/eureka/src/main/java/com/paymentchain/infrestrucuture/eureka/eurekaApplication.java
+++
@@ -1,8 +0,0 @@
-package com.paymentchain.infrestrucuture.eureka;
-
-public class eurekaApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        
-    }
-}
